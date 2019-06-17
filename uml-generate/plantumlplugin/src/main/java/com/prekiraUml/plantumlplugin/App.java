0c3aef53f2e824c4c1ed9cfa107b76a38bded04a
Prerana Kiran <prekira@hotmail.com>
2019-06-17 16:53:39 -0500

    changes

--- uml-generate/plantumlplugin/src/main/java/com/prekiraUml/plantumlplugin/App.java
+++ uml-generate/plantumlplugin/src/main/java/com/prekiraUml/plantumlplugin/App.java
@@ -1,7 +1,9 @@
 package com.prekiraUml.plantumlplugin;
 
-import com.google.gson.Gson;
+
 import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Hello world!
@@ -9,25 +11,64 @@ import java.io.IOException;
  */
 public class App 
 {
-    public static void main( String[] args ) throws IOException
+	public static String[] stereotypes = {"Audited", "interface", "enumeration"};
+    public static String[] classNamesToIgnore = {"Dto", "String", "Boolean"};
+    
+	
+	public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
+        JSONObject dtoMap = parseJson();
         
-        int ver = 1;
-        String jsonPath = "DtoMap"+ver+".json";
-        DtoMap[] dtoList = parseJsonFromFile(jsonPath);
-        //JsonObject jsonobj = new JsonObject();
-        print("hello");
+        //String pageName = dtoMap.getJSONObject("pageInfo").getString("pageName");
+        JSONArray dtoList = (dtoMap.getJSONArray("data"));
+        print(dtoList.getJSONObject(0).getString("className"));
     }
+	
+	public String getPropertiesFromDto(JSONObject dto) {
+		String propertyOfClass = "\n{\n";
+		String connectionsOfClass = "";
+		JSONArray propertyList = dto.getJSONArray("properties");
+		for (int i = 0; i < propertyList.length(); i++) {
+			propertyOfClass += "	+";
+			
+			
+		}
+	}
+	
+	/**
+	 * find if dtoobject is dto from type name
+	 * @param typeName full type name
+	 * @return if obj is dto
+	 */
+	public boolean isDto(String typeName) {
+		return typeName.substring(typeName.lastIndexOf(".") - "dto".length(), typeName.lastIndexOf(".")).toLowerCase().equals("dto");
+	}
+	
+	/**
+	 * extract class name from string of package directory
+	 * @param name of path
+	 * @return name of individual class
+	 */
+	public String getClassNameFromString(String name) {
+		return name.substring(name.lastIndexOf(".")+1, name.length()); 
+	}
     
+	public String getStereotypeFromDto(JSONObject dto) {
+		JSONArray stereotypeList = dto.getJSONArray("annotations");
+		for (int i; i < stereotypeList.length(); i++) {
+			
+		}
+	}
+	
     /*easier printing syntax*/
     public static void print(Object x) {
     	System.out.println(x.toString());
     }
     
-    
-    public static DtoMap[] parseJsonFromFile(String filename) {
-        Gson gson = new Gson();
-        return gson.fromJson(Data.getFileContentsAsString(filename), DtoMap[].class);
+    public static JSONObject parseJson() {
+    	int ver = 1;
+        String jsonPath = "DtoMap"+ver+".json";
+        return new JSONObject("{ \"data\": " + Data.getFileContentsAsString(jsonPath) + "}");
     }
 }
--- uml-generate/plantumlplugin/src/main/java/com/prekiraUml/plantumlplugin/DtoMap.java
+++ uml-generate/plantumlplugin/src/main/java/com/prekiraUml/plantumlplugin/DtoMap.java
@@ -5,7 +5,7 @@ public class DtoMap {
 	public String className = "";
 	public String fullClassName = "";
 	public Annotation annotations;
-	
+	public Property[] properties;
 	
 	
 	public String toString() {
@@ -15,7 +15,36 @@ public class DtoMap {
 	
 	public class Annotation {
 		public String Audited;
-		public String x;
+		public String isDto;
+		
+		public String notRequired;
+		public String isOptional;
+		
+
+		public String isMap;
+		public String mapKeyType;
+	}
+	
+	public class Property {
+		public String name;
+		public String dtoClassName;
+		public String type;
+		public Annotation[] annotations;
+		public DtoEnum dtoEnums;
+		
+	}
+	
+	public class DtoEnum {
+		public EnumValue value;
+	}
+	
+	public class EnumValue {
+		public String name;
+		public Value[] values;
+	}
+	
+	public class Value {
+		public String name;
 	}
 	
 }
